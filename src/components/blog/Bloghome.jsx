aafd36b38307ba0bac432d74aa73001554337740
nitish4singh <nitishsingh4748@gmail.com>
2023-09-29 11:58:27 +0545

    apichanges loding condition added

--- src/components/blog/Bloghome.jsx
+++ src/components/blog/Bloghome.jsx
@@ -5,21 +5,24 @@ import './blogcss.css';
 
 const BlogHome = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
   const cat = useLocation().search;
- 
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await axios.get(`/blogs/allBlogs`);
+        console.log(res.data); // Log the data
         setPosts(res.data);
+        setLoading(false); // Set loading to false after data is fetched
       } catch (err) {
         console.error(err);
+        setLoading(false); // Set loading to false in case of error
       }
     };
     fetchData();
   }, [cat]);
 
-  // Assuming you already have your getText and truncateText functions defined
   const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent;
@@ -29,28 +32,34 @@ const BlogHome = () => {
     if (text.length <= limit) return text;
     return text.substring(0, limit) + "...";
   };
+
   return (
     <div className="home1">
-      <div className="posts1">
-        {posts.map((post) => (
-          <div className="post1" key={post.id}>
-            <div className="img">
-              <img src={post.imgUrl1} alt="" />
+      {loading ? ( // Conditional rendering based on loading state
+        <p>Loading...</p>
+      ) : (
+        <div className="posts1">
+          {posts.map((post) => (
+            <div className="post1" key={post.id}>
+              <div className="img">
+                <img src={post.imgUrl1} alt="" />
+              </div>
+              <div className="content1">
+                <Link className="link1" to={`/post/${post.url}`}>
+                  <div className="title">{post.title}</div>
+                </Link>
+                <p>{truncateText(getText(post.desci), 50)}</p>
+                <Link className="read-more1" to={`/post/${post.url}`}>
+                  Read More
+                </Link>
+              </div>
             </div>
-            <div className="content1">
-              <Link className="link1" to={`/post/${post.url}`}>
-                <div className="title">{post.title}</div>
-              </Link>
-              <p>{truncateText(getText(post.desci), 50)}</p>
-              <Link className="read-more1" to={`/post/${post.url}`}>
-                Read More
-              </Link>
-            </div>
-          </div>
-        ))} 
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 export default BlogHome;
+
--- src/components/blog/blogsinglepage.jsx
+++ src/components/blog/blogsinglepage.jsx
@@ -3,43 +3,51 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 import moment from "moment";
 import DOMPurify from "dompurify";
+
 const Single = () => {
   const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(true); // Add loading state
   const location = useLocation();
   const postId = location.pathname.split("/")[2];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await axios.get(`/blogs/${postId}`);
         setPost(res.data);
+        setLoading(false); // Set loading to false after data is fetched
       } catch (err) {
         console.log(err);
+        setLoading(false); // Set loading to false in case of an error
+        // Handle the error (e.g., display an error message or redirect)
       }
     };
     fetchData();
   }, [postId]);
 
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
 
-  const getText = (html) =>{
-    const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
   return (
     <div className="single">
-      <div className="content">
-        <img src={post?.imgUrl2} alt="" />
-            <p>Posted {moment(post.date).fromNow()}</p>
-        <h1>{post.title}</h1>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(post.desci),
-          }}
-        ></p></div>
-          
-    
-      </div>
-  
-    
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="content">
+          <img src={post?.imgUrl2} alt="" />
+          <p>Posted {moment(post.date).fromNow()}</p>
+          <h1>{post.title}</h1>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(post.desci),
+            }}
+          ></p>
+        </div>
+      )}
+    </div>
   );
 };
+
 export default Single;
